ac2dd72e76c226d98089ba732cec50fa7b62908f
unibon4141 <vhcdyufg@gmail.com>
2024-05-13 23:56:57 +0900

    add: プロキシサーバーとしての処理を作成

--- app/controllers/HomeController.scala
+++ app/controllers/HomeController.scala
@@ -1,24 +1,40 @@
 package controllers
 
+import play.api.Configuration
 import javax.inject._
-import play.api._
-import play.api.mvc._
+import play.api.mvc.{Action, _}
+import play.api.libs.ws._
+import scala.concurrent.{ExecutionContext, Future}
+import scala.concurrent.duration.DurationInt
 
-/**
- * This controller creates an `Action` to handle HTTP requests to the
- * application's home page.
- */
 @Singleton
-class HomeController @Inject()(val controllerComponents: ControllerComponents) extends BaseController {
+class HomeController @Inject()(val controllerComponents: ControllerComponents, val ws: WSClient, val config: Configuration)(implicit ec: ExecutionContext) extends BaseController {
 
-  /**
-   * Create an Action to render an HTML page.
-   *
-   * The configuration in the `routes` file means that this method
-   * will be called when the application receives a `GET` request with
-   * a path of `/`.
-   */
-  def index() = Action { implicit request: Request[AnyContent] =>
-    Ok(views.html.index())
+  def proxy(path: String, qNames: List[String], qVals: List[String]): Action[AnyContent] = Action.async {
+    implicit request: Request[AnyContent] =>
+      val API_KEY = config.get[String]("api_key")
+      val baseUrl = config.get[String]("base_url")
+
+      val requestUrl = makeUrl(baseUrl, path, qNames, qVals)
+      val wsRequest: WSRequest = ws.url(requestUrl)
+      val complexRequest: WSRequest =
+        wsRequest.addHttpHeaders("Ocp-Apim-Subscription-Key" -> API_KEY)
+          .withRequestTimeout(100000.millis)
+      val futureResponse: Future[WSResponse] = complexRequest.get
+
+      for {
+        response <- futureResponse
+      } yield {
+        Ok(response.json)
+      }
   }
+
+  private def makeUrl(baseUrl: String, path: String, qNames: List[String], qVals: List[String]): String = {
+    var parameterStr = baseUrl + path + "?"
+    for (i <- qNames.indices) {
+      parameterStr += qNames(i) + "=" + qVals(i) + "&"
+    }
+    parameterStr
+  }
+
 }
--- build.sbt
+++ build.sbt
@@ -2,16 +2,20 @@ name := """eal-estate-proxy"""
 organization := "com.example"
 
 version := "1.0-SNAPSHOT"
+val circeVersion = "0.14.7"
 
 lazy val root = (project in file(".")).enablePlugins(PlayScala)
-
+val playV  = "3.0.2"
 scalaVersion := "2.13.14"
-
-libraryDependencies += guice
+libraryDependencies ++= Seq(
+  "io.circe" %% "circe-core",
+  "io.circe" %% "circe-generic",
+  "io.circe" %% "circe-parser"
+).map(_ % circeVersion)
+libraryDependencies += "org.playframework" %% "play"          % playV
+libraryDependencies += "org.playframework" %% "play-guice"    % playV
+libraryDependencies += "com.dripower" %% "play-circe" % "3014.1"
+//libraryDependencies += guice
 libraryDependencies += "org.scalatestplus.play" %% "scalatestplus-play" % "7.0.0" % Test
-
-// Adds additional packages into Twirl
-//TwirlKeys.templateImports += "com.example.controllers._"
-
-// Adds additional packages into conf/routes
-// play.sbt.routes.RoutesKeys.routesImport += "com.example.binders._"
+libraryDependencies += ws
+libraryDependencies += filters
